45f585ee781f621ac6090ab30daa2ec261d5ae23
Denis Davydov <macevoa@users.noreply.github.com>
2021-01-20 21:05:08 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+  if (n < 0) {
+    return (+(`${n * -1}`.split('').reverse().join('')));
+  } else {
+    return (+(`${n}`.split('').reverse().join('')));
+  }
 }
